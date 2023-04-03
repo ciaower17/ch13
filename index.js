dcc361ad7d5b269a9c68064e53127db1f6a6ca2e
ciaower17 <124397217+ciaower17@users.noreply.github.com>
2023-04-03 17:29:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+$(".mon").mouseover(function(){
+    $(".prices1").css("visibility","visible");
+    $(".mon").css("background-color","hsl(10, 86%, 75%)");
+});
+$(".mon").mouseout(function(){
+    $(".prices1").css("visibility","hidden");
+    $(".mon").css("background-color","hsl(10, 79%, 65%)");
+});
+
+$(".tue").mouseover(function(){
+    $(".prices2").css("visibility","visible");
+    $(".tue").css("background-color","hsl(10, 86%, 75%)");
+});
+$(".tue").mouseout(function(){
+    $(".prices2").css("visibility","hidden");
+    $(".tue").css("background-color","hsl(10, 79%, 65%)");
+});
+
+$(".thu").mouseover(function(){
+    $(".prices4").css("visibility","visible");
+    $(".thu").css("background-color","hsl(10, 86%, 75%)");
+});
+$(".thu").mouseout(function(){
+    $(".prices4").css("visibility","hidden");
+    $(".thu").css("background-color","hsl(10, 79%, 65%)");
+});
+
+$(".fri").mouseover(function(){
+    $(".prices5").css("visibility","visible");
+    $(".fri").css("background-color","hsl(10, 86%, 75%)");
+});
+$(".fri").mouseout(function(){
+    $(".prices5").css("visibility","hidden");
+    $(".fri").css("background-color","hsl(10, 79%, 65%)");
+});
+
+$(".sat").mouseover(function(){
+    $(".prices6").css("visibility","visible");
+    $(".sat").css("background-color","hsl(10, 86%, 75%)");
+});
+$(".sat").mouseout(function(){
+    $(".prices6").css("visibility","hidden");
+    $(".sat").css("background-color","hsl(10, 79%, 65%)");
+});
+
+$(".sun").mouseover(function(){
+    $(".prices7").css("visibility","visible");
+    $(".sun").css("background-color","hsl(10, 86%, 75%)");
+});
+$(".sun").mouseout(function(){
+    $(".prices7").css("visibility","hidden");
+    $(".sun").css("background-color","hsl(10, 79%, 65%)");
+});
+
+$(".wed").mouseover(function(){
+    $(".prices3").css("visibility","visible");
+    $(".wed").css("background-color","hsl(185, 21%, 68%)");
+});
+$(".wed").mouseout(function(){
+    $(".prices3").css("visibility","hidden");
+    $(".wed").css("background-color","hsl(186, 34%, 60%)");
+});
+
